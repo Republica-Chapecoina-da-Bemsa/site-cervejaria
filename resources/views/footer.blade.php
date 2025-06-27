c356a649e5a05e7cd8b1760c84fc0eb836345710
eduRossetti <edu.rossetti100@gmail.com>
2025-06-27 16:50:23 -0300

    estilizacao welcome page

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-light text-center text-lg-start mt-5 sticky-bottom">
+<footer class="bg-light text-center text-lg-start mt-5">
     <div class="text-center p-3" style="background-color: #ddd;">
         2024 Dom Guri - Todos os direitos reservados
     </div>
